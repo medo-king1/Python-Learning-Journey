e15d8140995202cac1c0123be1f7f48c0de71e96
Medo King <ahmedx9940@gmail.com>
2026-06-02 18:07:15 +0300

    Complete calculator project with f-strings

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
-# First code
-# String print
-print("Medo King Say : Hello")
+# input first number
+num1 = int(input('Enter first number : '))
 
-# Integer print
-print(5)
+# input operator
+op = input('Enter operator +,-,*,/ : ')
 
-#Float Print
-print(10.5)
+# input second number
+num2 = int(input('Enter second number : '))
+# conditions , input result
+if op == "+":
+    print(f" Result is :{num1 + num2}")
 
-print(type("Ahmed"))
+elif op == "-":
+    print(f" Result is :{num1 - num2}")
 
-#variables
+elif op == "*" :
+    print(f" Result is :{num1 * num2}")
 
-age = 18      # قيمة age تساوي 18
+elif op == "/" :
+    print(f" Result is :{num1 / num2}")
 
-#تطبيق على print , type , data types , variables
-print(type(age)) # النتيجة ستكون <class 'int'>
+else:
+    print("Error")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # First code
+# # String print
+# print("Medo King Say : Hello")
+
+# # Integer print
+# print(5)
+
+# #Float Print
+# print(10.5)
+
+# print(type("Ahmed"))
+
+# #variables
+
+# age = 18      # قيمة age تساوي 18
+
+# #تطبيق على print , type , data types , variables
+# print(type(age)) # النتيجة ستكون <class 'int'>
