5e9eea631b39f374d756cf9980cc980a4c56ea77
Medo King <ahmedx9940@gmail.com>
2026-06-02 18:22:44 +0300

    fix an error

--- main.py
+++ main.py
@@ -1,38 +1,3 @@
-# input first number
-num1 = int(input('Enter first number : '))
-
-# input operator
-op = input('Enter operator +,-,*,/ : ')
-
-# input second number
-num2 = int(input('Enter second number : '))
-# conditions , input result
-if op == "+":
-    print(f" Result is :{num1 + num2}")
-
-elif op == "-":
-    print(f" Result is :{num1 - num2}")
-
-elif op == "*" :
-    print(f" Result is :{num1 * num2}")
-
-elif op == "/" :
-    print(f" Result is :{num1 / num2}")
-
-else:
-    print("Error")
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -54,3 +19,34 @@ else:
 
 # #تطبيق على print , type , data types , variables
 # print(type(age)) # النتيجة ستكون <class 'int'>
+
+
+
+# calculator project
+#--------------------------------------------------------#
+
+# *TODO*: Upgrade inputs from int to float to support decimal numbers later
+
+# # input first number
+# num1 = int(input('Enter first number : '))
+
+# # input operator
+# op = input('Enter operator +,-,*,/ : ')
+
+# # input second number
+# num2 = int(input('Enter second number : '))
+# # conditions , input result
+# if op == "+":
+#     print(f" Result is :{num1 + num2}")
+
+# elif op == "-":
+#     print(f" Result is :{num1 - num2}")
+
+# elif op == "*" :
+#     print(f" Result is :{num1 * num2}")
+
+# elif op == "/" :
+#     print(f" Result is :{num1 / num2}")
+
+# else:
+#     print("Error")
