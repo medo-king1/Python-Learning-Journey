8bd5ae8988c3de865b678a77dadc39b98d3bf90d
Medo King <ahmedx9940@gmail.com>
2026-06-16 18:17:57 +0300

    create folder of first things i learned

---
+++ first things.py
@@ -0,0 +1,18 @@
+# First code
+# String print
+print("Medo King Say : Hello")
+
+# Integer print
+print(5)
+
+#Float Print
+print(10.5)
+
+print(type("Ahmed"))
+
+#variables
+
+age = 18      # قيمة age تساوي 18
+
+#تطبيق على print , type , data types , variables
+print(type(age)) # النتيجة ستكون <class 'int'>
