eaa86491e4cbe3e6c201ca13bd490428b88550fb
Medo King <ahmedx9940@gmail.com>
2026-06-16 18:18:10 +0300

    clear

--- main.py
+++ main.py
@@ -1,52 +1,18 @@
-#No new any thing today
+# First code
+# String print
+print("Medo King Say : Hello")
 
-#----------------------------------------------
-# # First code
-# # String print
-# print("Medo King Say : Hello")
+# Integer print
+print(5)
 
-# # Integer print
-# print(5)
+#Float Print
+print(10.5)
 
-# #Float Print
-# print(10.5)
+print(type("Ahmed"))
 
-# print(type("Ahmed"))
+#variables
 
-# #variables
+age = 18      # قيمة age تساوي 18
 
-# age = 18      # قيمة age تساوي 18
-
-# #تطبيق على print , type , data types , variables
-# print(type(age)) # النتيجة ستكون <class 'int'>
-
-
-
-# calculator project
-#--------------------------------------------------------#
-
-# *TODO*: Upgrade inputs from int to float to support decimal numbers later
-
-# # input first number
-# num1 = int(input('Enter first number : '))
-
-# # input operator
-# op = input('Enter operator +,-,*,/ : ')
-
-# # input second number
-# num2 = int(input('Enter second number : '))
-# # conditions , input result
-# if op == "+":
-#     print(f" Result is :{num1 + num2}")
-
-# elif op == "-":
-#     print(f" Result is :{num1 - num2}")
-
-# elif op == "*" :
-#     print(f" Result is :{num1 * num2}")
-
-# elif op == "/" :
-#     print(f" Result is :{num1 / num2}")
-
-# else:
-#     print("Error")
+#تطبيق على print , type , data types , variables
+print(type(age)) # النتيجة ستكون <class 'int'>
