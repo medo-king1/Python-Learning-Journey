73afdb9a0d9e1474d2c941a9ca7b673bcdf3a229
Medo King <ahmedx9940@gmail.com>
2026-06-17 18:32:16 +0300

    lists / القوائم

---
+++ main.py
@@ -0,0 +1,35 @@
+# lists : القوائم
+
+students = ["Ahmed", "Mohamed", "Mahmoud"]
+
+# lists tools : أدوات في القوائم
+
+# #append At the END! في نهاية القائمة
+# print(f"Before {students}")
+# students.append("Adel")
+# print(f"After {students}")
+
+# #insert (Index , object) إضافة في مكان محدد
+# print(f"Before {students}")
+# students.insert(0 , "Adel")
+# print(f"After {students}")
+
+# #Remove مسح العنصر باسمه من القائمة
+# print(f"Before {students}")
+# students.remove(students[0])
+# print(f"After {students}")
+
+# #del أمر مسح صريح للعنصر من الذاكرة بالـ Index
+# print(f"Before {students}")
+# del students[0]
+# print(f"After {students}")
+
+# #pop أساسها مسح العنصر الأخير ولكن تستطيع ادخال index
+# print(f"Before {students}")
+# students.pop(0)
+# print(f"After {students}")
+
+# #clear تنظيف اللستة بالكامل وتركها قائمة فارغة []
+# print(f"Before {students}")
+# students.clear()
+# print(f"After {students}")
