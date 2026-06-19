915fab645cb41831e304cc73adf28fe331001750
Medo King <ahmedx9940@gmail.com>
2026-06-19 22:07:03 +0300

    The Journey Begins / بداية الرحلة

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-# function / الدوال
-# def greet():
-#     print("Hello Ahmed")
-# greet()
-# greet()
+# - The journey is far from over; this is just the beginning!
+# - Still got a lot to learn in the coming period to lock down all the basics.
+# - Keeping at it until I master every important and semi-important concept in programming. 🚀🔥
 
-#Parameter / المعامل
-# def Say_Hello(name):
-#     print(f"Hello {name}")
-# Say_Hello()
+# - الرحلة لسة مخلصتش، دي مجرد البداية!
+# - لسة فيه حاجات ثانية كتير هتعلمها الفترة الجاية عشان أقفل كل الأساسيات.
+# - مستمر لحد ما أفرم كل المعلومات المهمة والشبه مهمة في البرمجة.🚀🔥
 
-#return
-# def add_numbers(num1, num2):
-#     result = num1 + num2
-#     return result
-# total = add_numbers(5, 10)
-# print(total)
+# The Journey BY : Ahmed Ibrahim (Medo_King)
